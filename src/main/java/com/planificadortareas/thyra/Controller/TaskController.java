bcf40e6f281258f6c8d10ed14d9e88b33bb51230
SneyderRodriguez <sneyder2112_@hotmail.com>
2026-09-15 17:05:22 -0500

    Endpoint GET

---
+++ src/main/java/com/planificadortareas/thyra/Controller/TaskController.java
@@ -0,0 +1,24 @@
+package com.planificadortareas.thyra.Controller;
+
+import com.planificadortareas.thyra.Model.Task;
+import com.planificadortareas.thyra.Repository.TaskRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+@RestController
+@RequestMapping("/api/tasks")
+public class TaskController {
+    private final TaskRepository taskRepository;
+
+    public TaskController(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+}
