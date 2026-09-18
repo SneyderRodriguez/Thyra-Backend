1338fb2cf478280253fe471960cbb52280cfe673
SneyderRodriguez <sneyder2112_@hotmail.com>
2026-09-17 21:14:46 -0500

    Corrección CorsConfig

--- src/main/java/com/planificadortareas/thyra/Config/CorsConfig.java
+++ src/main/java/com/planificadortareas/thyra/Config/CorsConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Value("https://thyratask.netlify.app/")
+    @Value("${cors.allowed-origins}")
     private String allowedOrigins;
 
     @Override
